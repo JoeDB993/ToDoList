95499a6931346b75c1c48cecaacfa09422af0990
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-27 09:29:46 -0600

    Agregar archivos de proyecto.

---
+++ ToDoList/Models/Tarea.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace ToDoList.Models
+{
+    public class Tarea
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public string Nombre { get; set; }
+        [NotNull]
+        public string Descripcion { get; set; }
+        public string Estado { get; set; } // "Por hacer", "En progreso", "Finalizada" 
+    }
+}
