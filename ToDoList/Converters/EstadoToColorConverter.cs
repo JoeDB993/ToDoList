d15519eabff272cb945a7964cd9604b7aee63afc
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-28 20:39:52 -0600

    Se realizaron cambios en la UI

---
+++ ToDoList/Converters/EstadoToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace ToDoList.Converters
+{
+    public class EstadoToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string estado)
+            {
+                return estado switch
+                {
+                    "Terminada" => Colors.Green,   // Verde para tareas finalizadas
+                    "En proceso" => Colors.Blue, // Azul para tareas en progreso
+                    "Por hacer" => Colors.Red,      // Rojo para tareas por hacer
+                    _ => Colors.Gray,               // Color gris para estados desconocidos
+                };
+            }
+            return Colors.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
